5e9abd6592cd5547da5f980778ecdf0c600f939d
ancirankovicc-beep <ancirankovicc@gmail.com>
2025-12-17 00:29:45 +0100

    Update config.js

--- config.js
+++ config.js
@@ -32,7 +32,7 @@ class EnterpriseConfigurationManager {
             lavalink: {
                 host: EnvironmentVariableProcessor.LAVALINK_HOST || "194.58.66.44", 
                 port: EnvironmentVariableProcessor.LAVALINK_PORT || 3660,       
-                password: EnvironmentVariableProcessor.LAVALINK_PASSWORD || "AYANO LAVA", 
+                password: EnvironmentVariableProcessor.LAVALINK_PASSWORD || "AYANO", 
                 secure: EnvironmentVariableProcessor.LAVALINK_SECURE === 'true' || false
             },           
             
@@ -101,6 +101,7 @@ module.exports = primaryApplicationConfiguration;
  * Never share your bot token or database URI publicly!
  * Use environment variables in production!
  */
+
 
 
 

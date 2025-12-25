56f5ec6f75ef6886cfb0b27b1314068ebe7e8f99
ancirankovicc-beep <ancirankovicc@gmail.com>
2025-12-25 15:41:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-require('./main');
-require('./shiva'); 
-const path = require('path');
-const express = require("express");
+import "./main.js";
+import "./shiva.js";
+
+import path from "path";
+import express from "express";
+import { fileURLToPath } from "url";
+
 const app = express();
 const port = 8888;
-app.get('/', (req, res) => {
-    const imagePath = path.join(__dirname, 'index.html');
+
+/* __dirname fix za ESM */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.get("/", (req, res) => {
+    const imagePath = path.join(__dirname, "index.html");
     res.sendFile(imagePath);
 });
+
 app.listen(port, () => {
     console.log(`🔗 Listening to GlaceYT : http://localhost:${port}`);
 });
